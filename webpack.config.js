0f10cc0f0b5788cd78ac992ddd1251dc712667a3
Jacob&Ivy <jacob.wd8816@gmail.com>
2017-03-22 11:51:41 +0800

    - init aslant

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,43 @@
 var path = require('path');
 
 module.exports = {
-  devtool: "inline-source-map",
-  entry: './app/index.js',
+  devtool: 'inline-source-map',
+  entry: {
+    main: './app/index.js'
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/'
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [['es2015', {modules: false}]],
+          plugins: [
+            'syntax-dynamic-import',
+            'transform-async-to-generator',
+            'transform-regenerator',
+            'transform-runtime'
+          ]
+        }
+      }]
+    }]
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'lib'),
+      'node_modules'
+    ]
+  },
   devServer: {
     port: 8080,
     hot: true,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     historyApiFallback: {
       index: 'index.html'
     }
